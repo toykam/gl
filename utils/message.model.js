6d772b96de4e3039d16ad1e4aacd518668aa343b
toykam <mwork063@gmail.com>
2020-12-18 00:57:03 +0100

    made some update

--- utils/message.model.js
+++ utils/message.model.js
@@ -1,13 +1,15 @@
 const moment = require("moment");
 const uuid = require('uuid')
 
-function formatMessage(username, message) {
+function formatMessage(user, message) {
     return {
-        username,
-        message,
-        time: moment().format('h:mm a'),
-        sender: username,
-        _id: uuid.v4()
+        text: message,
+        created_at: moment().format('h:mm a'),
+        sender: user.name,
+        _id: uuid.v4(),
+        user: {
+            _id: user.id, name: user.name
+        }
     };
 }
 
